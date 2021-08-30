2669265b6c2e7a0f512eec72dadb328a69cd6682
randmaru <git@randma.ru>
2021-08-30 22:25:41 +0300

    feat: added cycle "for" & other logical

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ module.exports = function check(str, bracketsConfig) {
 	const openBrackets = ["(", "[", "{", "1", "3", "5"],
 		closBrackets = [")", "]", "}", "2", "4", "6"],
 		stack = [];
+
+	for (let i = 0; i < str.length; i++) {
+		if (openBrackets.indexOf(str[i]) != -1) {
+			stack.push(str[i]);
+		} else {
+			if (closBrackets.indexOf(str[i]) != -1 && stack.length != 0) {
+				if (
+					openBrackets.indexOf(stack[stack.length - 1]) ==
+					closBrackets.indexOf(str[i])
+				) {
+					stack.pop();
+				}
+			} else {
+				str[i] == stack[stack.length - 1]
+					? stack.pop()
+					: stack.push(str[i]);
+			}
+		}
+	}
+
+	return stack.length == 0 ? true : false;
 };
